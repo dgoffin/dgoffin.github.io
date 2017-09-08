6797e37810ee19cf4a80cdc1b62b0f8ebdcda4b7
dgoffin <darren.goffin@gmail.com>
2017-09-08 13:01:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var json = {"items": [
  }
 ]};
 
-var news = document.getElementsByClassName("news-story")[0];
+var news = document.getElementsByClassName("proposals-list")[0];
 var items = json.items;
 for(var i = 0; i < items.length; i++) {
     var h5 = document.createElement("h5");
