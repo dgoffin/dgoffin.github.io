fe9f2271458e4cb936526bfefa0cf47b0254b515
dgoffin <darren.goffin@gmail.com>
2017-09-08 14:53:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function renderBody () {
 
 
             var node = document.createElement("LI");
-    var textnode = document.createTextNode("Water");
+    var textnode = document.createTextNode("ABC");
     node.appendChild(textnode);
     document.getElementById("myList").appendChild(node);
     
