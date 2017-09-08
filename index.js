a6f920dc151862f95aea2869b8b74703281fec7a
dgoffin <darren.goffin@gmail.com>
2017-09-08 13:48:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 
+/** Holds the primary data used on this page: metadata about Swift Evolution proposals. */
+var proposals
+
 var myObj, myJSON, text, obj;
 
 //Storing data:
 
+init()
+
+/** Primary entry point. */
+function init () {
     
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+    var req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.name;
+        proposals = JSON.parse(this.responseText);
+        document.getElementById("demo").innerHTML = proposals.name;
     }
-};
-xmlhttp.open("GET", "json.json", true);
-xmlhttp.send();
+    };
+    req.open('get', 'json.json');
+    req.send();
+    
+}
+
+    
+
     
     
